2c58ad8b9b40f31a0b972626a14b4f8f02713149
RamonPapes <ramonpapes@gmail.com>
2026-09-24 09:59:59 -0300

    Testa Pedido Service

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
-from src.models.desconto import DescontoVIP
+from src.models.desconto import DescontoVIP, DescontoNormal, DescontoPremium
 from src.models.pedido import Pedido
+from src.services.pedido_service import PedidoService
 
 if __name__ == "__main__":
-    pedido = Pedido("Leonardo", DescontoVIP())
+    service = PedidoService()
 
-    valor_final = pedido.valor_final(100)
-    print(f"Cliente: {pedido.cliente}")
-    print(f"Valor final: {valor_final}")
+    """Criando pedidos e aplicando descontos"""
+    pedido1 = Pedido("Cliente A", DescontoNormal())
+    pedido1.valor_original = 100.0
+
+    pedido2 = Pedido("Cliente B", DescontoVIP())
+    pedido2.valor_original = 200.0
+
+    pedido3 = Pedido("Cliente C", DescontoPremium())
+    pedido3.valor_original = 300.0
+
+    service.adicionar_pedido(pedido1)
+    service.adicionar_pedido(pedido2)
+    service.adicionar_pedido(pedido3)
+
+    service.processar_pedidos()
+
+    
